1dbf85291298e24860b79c3cef357b8aba73d79c
Reginaldo Santos <regi_csf@hotmail.com>
2016-10-19 15:20:08 -0300

    Finalização do SeqAG
    Este commit consiste na finalização deste algoritmo sequencial.

--- SeqAG/src/auxiliaries/Configuration.h
+++ SeqAG/src/auxiliaries/Configuration.h
@@ -8,20 +8,23 @@
 #ifndef AUXILIARIES_CONFIGURATION_H_
 #define AUXILIARIES_CONFIGURATION_H_
 
-const int NCHROMOSOMES = 50; // must be even ('cause of crossover)
-const int NGENERATIONS = 500;
+/*Genetic algorithm parameters*/
+const int NCHROMOSOMES = 500; // must be even ('cause of crossover)
+const int NGENERATIONS = 1000;
 const double CROSSOVERRATE = 0.8;
 const double MUTATIONRATE = 0.01;
 const double SD = 0.1;
 const int RANK = 3;
 
+/*Output format parameters*/
 const int PRECISION = 3;
 const int WIDTH = PRECISION + 4;
 
+/*Auxiliary classes*/
 class Ackley {
 public:
 	static const int ID = 0;
-	static const int NGENES = 4;
+	static const int NGENES = 500;
 	static const int INFIMUM = -40;
 	static const int MAXIMUM = 40;
 };
