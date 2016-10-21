8430acfc7169c3e8ba2803a12ced92852cf450ed
Reginaldo Santos <regicsf2010@gmail.com>
2016-10-20 21:53:21 -0300

    Algumas modificações
    Pequenas modificações foram adicionadas no código.

--- SeqAG/src/main.cpp
+++ SeqAG/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 #include "auxiliaries/Configuration.h"
 #include "ag/MoonSlow.h"
@@ -6,12 +7,17 @@ using namespace std;
 
 int main(void) {
 	MoonSlow ml(Ackley::ID);
-	Population *p = ml.run();
-	int idx = p->getFittest();
 
-	p->getChromosome(idx)->print(p->getFunctionID());
+	auto start = chrono::steady_clock::now();
+	Population *p = ml.run();
+	auto finish = chrono::steady_clock::now();
+
+	//double elapsed = chrono::duration_cast<chrono::milliseconds>(finish - start).count();
+	double elapsed = chrono::duration_cast<chrono::duration<double>>(finish - start).count();
+
+	cout << "Time: " << elapsed << " (s)" << endl;
 
 	delete p;
-	cout << "END" << endl;
+	cout << "--END MOONSLOW--" << endl;
 	return 0;
 }
